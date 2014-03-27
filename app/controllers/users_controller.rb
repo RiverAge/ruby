ea41904549f3f36c0f4f08c035df593f04bb1456
RiverAge <RiverAge@163.com>
2014-03-27 22:19:03 +0800

    user sign up function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,9 @@ class UsersController < ApplicationController
 
   def create
 
-    @user = User.new(params.require(:user).permit(:name, :email))
+    @user = User.new(params.require(:user).permit(:name, :email, :password, :password_confirmation))
     if @user.save
-
+      redirect_to @user
     else
       render 'new'
     end
