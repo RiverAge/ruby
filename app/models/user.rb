f9a69c8a14e9a6e815eba8b1b6cc6c6b8540e649
RiverAge <riverage@163.com>
2014-04-19 17:34:30 +0800

    micropost model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-
   has_secure_password
+  has_many :microposts, dependent: :destroy
   #before_save {|user| user.email = email.downcase}
   before_save {self.email.downcase!}
   before_save :create_remember_token
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140419043353) do
+ActiveRecord::Schema.define(version: 20140419082739) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -23,9 +23,16 @@ ActiveRecord::Schema.define(version: 20140419043353) do
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
-  create_table "posts", force: true do |t|
-    t.string   "title"
-    t.text     "text"
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "micrposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
