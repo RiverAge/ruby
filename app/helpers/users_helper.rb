0635fd367d6d484ccac5b868c9690a93843142f8
RiverAge <riverage@163.com>
2014-06-11 21:44:45 +0800

    add reset password function

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -42,7 +42,7 @@ module UsersHelper
   def signed_in_user
     if (!signed_in?)
       store_location
-      redirect_to signin_path
+      redirect_to login_path
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,14 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: {minimum: 6}
   validates :password_confirmation, presence: true
 
+  def send_password_reset
+    self.password_reset_token = SecureRandom.urlsafe_base64
+    self.password_reset_sent_at = Time.zone.now
+    self.save validate: false
+
+    UserMailer.password_reset(self).deliver
+  end
+
   private
   def create_remember_token
     self.remember_token = SecureRandom.urlsafe_base64
