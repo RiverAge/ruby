912f250cb98e476769ce116108193b85141ddf5d
RiverAge <riverage@163.com>
2014-12-06 22:04:00 +0800

    add info when creat new shared file

--- app/controllers/shared_files_controller.rb
+++ app/controllers/shared_files_controller.rb
@@ -12,6 +12,7 @@ class SharedFilesController < ApplicationController
   def create
     @shared_file = SharedFile.new(params.require(:shared_file).permit(:title, :summary, :category, :path))
     if @shared_file.save
+      flash[:info] = t "shared_file.controller.create_success"
       redirect_to shared_files_path
     else
       #创建文件失败时，由于@categories的生命周期已经结束，重新渲染new模板时，@categories已经没有值了，所以就没有下拉列表了
