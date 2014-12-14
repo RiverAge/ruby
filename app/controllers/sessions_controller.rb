228ccd874677352f7a2feeeaed89fec48b03a036
RiverAge <riverage@163.com>
2014-12-14 13:34:31 +0800

    add user helper -> logged_in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,10 @@ class SessionsController < ApplicationController
     name_or_email = params[:session][:name].downcase
     user = User.find_by(email: name_or_email) || User.find_by(name: name_or_email)
     if user && user.authenticate(params[:session][:password])
-  #  	sign_in_with_status user, (params[:session][:rememberme] == '1')
-			log_in user
-    	flash[:info] = t "user.controller.login_success"
-			redirect_to user
+  #  sign_in_with_status user, (params[:session][:rememberme] == '1')
+      log_in user
+      flash[:info] = t "user.controller.login_success"
+      redirect_to user
     else
       flash.now[:danger] = t "user.controller.wrong_username_or_password"
       render 'new'
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,64 +1,64 @@
 module UsersHelper
-  def gravatar_for(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-
-  end
-
-  def remember_me_next_time(user)
-    cookies.permanent[:remember_token] == user.remember_token
-  end
-
-  def sign_in_with_status(user, status)
-    if status
-      cookies.permanent[:remember_token] = user.remember_token
-    else
-      session[:remember_token] = user.remember_token
-    end
-    self.current_user = user
-  end
-
-#  def sign_in_with_session(user)
-#    session[:remember_token] = user.remember_token
-#    self.current_user = user
-#  end
-
-  def signed_in?
-    !current_user.nil?
-  end
-
-  def current_user=(user)
-    @current_user = user
-  end
-
-  def current_user?(user)
-    user == current_user
-  end
-
-  def current_user
-    @current_user = @current_user || User.find_by_remember_token(session[:remember_token]) || User.find_by_remember_token(cookies[:remember_token])
-  end
-
-  def redirect_back_or(default)
-    redirect_to session[:return_to] || default
-    session.delete(:return_to)
-  end
-
-  def store_location
-    session[:return_to] = request.fullpath
-  end
-
-  def signed_in_user
-    if (!signed_in?)
-      store_location
-      redirect_to login_path
-    end
-  end
-
-  def correct_user
-    @user = User.find(params[:id])
-    if (!current_user?(@user))
-      redirect_to root_path
-    end
-  end
+#   def gravatar_for(user)
+#     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+#
+#   end
+#
+#   def remember_me_next_time(user)
+#     cookies.permanent[:remember_token] == user.remember_token
+#   end
+#
+#   def sign_in_with_status(user, status)
+#     if status
+#       cookies.permanent[:remember_token] = user.remember_token
+#     else
+#       session[:remember_token] = user.remember_token
+#     end
+#     self.current_user = user
+#   end
+#
+# #  def sign_in_with_session(user)
+# #    session[:remember_token] = user.remember_token
+# #    self.current_user = user
+# #  end
+#
+#   def signed_in?
+#     !current_user.nil?
+#   end
+#
+#   def current_user=(user)
+#     @current_user = user
+#   end
+#
+#   def current_user?(user)
+#     user == current_user
+#   end
+#
+#   def current_user
+#     @current_user = @current_user || User.find_by_remember_token(session[:remember_token]) || User.find_by_remember_token(cookies[:remember_token])
+#   end
+#
+#   def redirect_back_or(default)
+#     redirect_to session[:return_to] || default
+#     session.delete(:return_to)
+#   end
+#
+#   def store_location
+#     session[:return_to] = request.fullpath
+#   end
+#
+#   def signed_in_user
+#     if (!signed_in?)
+#       store_location
+#       redirect_to login_path
+#     end
+#   end
+#
+#   def correct_user
+#     @user = User.find(params[:id])
+#     if (!current_user?(@user))
+#       redirect_to root_path
+#     end
+#   end
 
 end
